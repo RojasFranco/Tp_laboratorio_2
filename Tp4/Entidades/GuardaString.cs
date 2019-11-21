9fccc0f21e4b7dcc81b4131387101222d4e609a6
Franco <rojas.franco.93@gmail.com>
2019-11-21 14:18:17 -0300

    ...

---
+++ Tp4/Entidades/GuardaString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class GuardaString
+    {
+        public static bool Guardar(this string texto, string archivo)
+        {
+            bool append;
+            bool retorno;
+
+            if (File.Exists(archivo))
+            {
+                append = true;
+            }            
+            else
+            {
+                append = false;
+            }
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(archivo, append))
+                {
+                    writer.Write(texto);
+                    retorno = true;
+                }
+            }            
+            catch(Exception err)
+            {
+                retorno = false;
+            }
+            return retorno;
+        }
+    }
+}
